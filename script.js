dcf45c68b2c89f01c969d70cecd1c915d64bf4f8
Angel Tashev <angel.tashev.15@gmail.com>
2021-04-15 16:38:27 +0300

    Added websocket integration

--- script.js
+++ script.js
@@ -4,51 +4,82 @@ const telegramEndpoint = `https://api.telegram.org/bot${botId}`;
 const krakenEndpoint = 'https://api.kraken.com/0/public/';
 const ticker = krakenEndpoint + 'Ticker?pair=XBTEUR';
 let myApp;
-let lastPrice;
+let lastPrice = 0;
 let currentPrice;
 const diffPrice = 50;
 
+const regex = /"(.*?)"/;
+
+const webSocketUrl = "wss://ws.kraken.com/";
+var globalIsWebSocketOpen = false;
+
 // Array of Prices - Check Price every minute and compare
 
+function openWebSocket() {
+  
+    socket = new WebSocket(webSocketUrl);
+    socket.onopen = function () {
+      outputConsoleMessage("Service started! :)")
+      globalIsWebSocketOpen = true;
+      sendWebSocketMessage();
+    };
+  
+    socket.onclose = function () {
+      globalIsWebSocketOpen = false;
+    };
+  
+    socket.onmessage = function (message) {
+      
+      const data = message.data.match(regex);
+      const price = Number(data[1]);
+      if (!Number.isNaN(price)) {
+        currentPrice = price;
+        setPrice();
+      }
+      // TODO CheckPrice [337,[["52790.10000","0.01594846","1618413792.734278","b","l",""]],"trade","XBT/EUR"]
+    };
+  }
+
+  function sendWebSocketMessage() {
+
+    if (globalIsWebSocketOpen) {
+      var message = 
+      '{"event":"subscribe", "subscription":{"name":"trade"}, "pair":["XBT/EUR"]}';
+  
+      if (message !== "") {
+        outputConsoleMessage('Subscribing to Kraken...');
+        socket.send(message);
+      }
+      else {
+        alert("An error occured");
+      }
+    }
+    else {
+      alert("Start service first");
+    }
+  }
+
 async function startProcess() {
-    outputConsoleMessage('Starting Process');
-    outputConsoleMessage('Difference price: ' + diffPrice);
-    await fetchPrice();
-    lastPrice = currentPrice;
-    myApp = setInterval(getPrice, 1000);
+
+    openWebSocket();
+
+    // TODO Show difference price
+
 }
 
-async function getPrice() {
-
-    await fetchPrice();
+async function setPrice() {
 
     let diff = currentPrice - lastPrice;
-    // outputConsoleMessage('(Internal) Difference: €' + diff.toFixed(2));
+    console.log(diff);
     if (Math.abs(diff) >= diffPrice) {
         outputConsoleMessage((1 / currentPrice).toFixed(10));
         let percentageDiff = ((currentPrice / lastPrice) * 100) - 100;
         let message = `Price difference: €${diff.toFixed(2)}`;
         message += `    Percentage: ${percentageDiff.toFixed(2)}`;
         message += `    From €${lastPrice} to €${currentPrice}`;
-        // outputConsoleMessage(mes sage);
-        sendMessage(message);
         lastPrice = currentPrice;
     }
 
-}
-
-async function fetchPrice() {
-    // outputConsoleMessage('Updating Price...');
-    await fetch(ticker).then(res => res.json())
-    .then(res => {
-        const exchangePrice = res.result['XXBTZEUR'].c[0];
-        currentPrice = exchangePrice;
-    });
-}
-
-function sendMessage(message) {
-    // fetch(`${telegramEndpoint}/sendMessage?chat_id=${chatId}&text=${message}`,
-    // { method: 'POST' });
 }
 
 function outputConsoleMessage(message) {
